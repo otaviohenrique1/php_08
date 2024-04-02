d30847862b027897f4c6e97ffdbc22a84810b776
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-04-02 17:35:10 -0300

    commit 4

---
+++ erros.php
@@ -0,0 +1,12 @@
+<?php
+
+// ini_set('error_reporting', E_ALL);
+error_reporting(E_ALL);
+
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/log/minha-aplicacao');
+
+echo $variavel;
+echo $array[12];
+// echo CONSTANTE;
